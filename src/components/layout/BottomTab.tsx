7045af389ad6292d0d8a8a53b4e8e0cf7ff9928e
이강민 <lee.kangmin@postype.com>
2023-03-06 16:00:07 +0900

    BB-0005 feat: 아이콘 변경

--- src/components/layout/BottomTab.tsx
+++ src/components/layout/BottomTab.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Box, Text, Icon, HStack, Center, Pressable } from "native-base";
-import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Entypo,
+  Ionicons,
+} from "@expo/vector-icons";
 
 const BottomTab = () => {
   const [selected, setSelected] = React.useState(1);
   return (
     <Center position="absolute" left="0" right="0" bottom="0">
-      <Box flex={1} bg="white" safeAreaTop width="100%" alignSelf="center">
+      <Box flex={1} bg="white" width="100%" alignSelf="center" safeArea>
         <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
           <Pressable
             opacity={selected === 0 ? 1 : 0.5}
@@ -39,12 +44,12 @@ const BottomTab = () => {
             <Center>
               <Icon
                 mb="1"
-                as={<MaterialIcons name="search" />}
+                as={<Ionicons name="bookmark" size={24} color="black" />}
                 color="white"
                 size="sm"
               />
               <Text color="white" fontSize="12">
-                Search
+                Bookmark
               </Text>
             </Center>
           </Pressable>
@@ -66,7 +71,7 @@ const BottomTab = () => {
                 size="sm"
               />
               <Text color="white" fontSize="12">
-                Cart
+                Proposal
               </Text>
             </Center>
           </Pressable>
@@ -79,16 +84,12 @@ const BottomTab = () => {
             <Center>
               <Icon
                 mb="1"
-                as={
-                  <MaterialCommunityIcons
-                    name={selected === 3 ? "account" : "account-outline"}
-                  />
-                }
+                as={<Entypo name="dots-three-horizontal" color="black" />}
                 color="white"
                 size="sm"
               />
               <Text color="white" fontSize="12">
-                Account
+                Settings
               </Text>
             </Center>
           </Pressable>
