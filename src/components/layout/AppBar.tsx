381ccb51a962c7403b018a853355925df16efe85
이강민 <lee.kangmin@postype.com>
2023-03-10 12:21:21 +0900

    BB-0013 fix: 상단 탭 수정

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -6,6 +6,8 @@ import {
   useColorMode,
   Switch,
   Text,
+  VStack,
+  Pressable,
 } from "native-base";
 import { MaterialCommunityIcons, Feather } from "@expo/vector-icons";
 import LogoIcon from "../icons/LogoIcon";
@@ -14,59 +16,57 @@ const AppBar = () => {
   return (
     <HStack
       px={4}
-      shadow={6}
-      bgColor="white"
       justifyContent="space-between"
       alignItems="center"
       w="100%"
       _dark={{ bg: "blueGray.900" }}
       _light={{ bg: "blueGray.50" }}
     >
-      <HStack>
-        <LogoIcon />
-        <Text>Bookito</Text>
-      </HStack>
-      <ToggleDarkMode />
+      <LogoIcon size={9} />
+      <ToggleButtonDarkMode />
     </HStack>
   );
 };
 
-function ToggleDarkMode() {
+function ToggleButtonDarkMode() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <HStack space={2} alignItems="center">
-      {colorMode === "light" && (
-        <Icon
-          mb="1"
-          as={
-            <IconButton icon={<Feather name="sun" size={24} color="black" />} />
-          }
-          size="md"
-          alignSelf="center"
-        />
-      )}
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      {colorMode !== "light" && (
-        <Icon
-          mb="1"
-          as={
-            <MaterialCommunityIcons
-              name="weather-night"
-              size={24}
-              color="black"
-            />
-          }
-          size="md"
-          alignSelf="center"
-        />
-      )}
-    </HStack>
+    <Pressable
+      onPress={toggleColorMode}
+      rounded="full"
+      borderColor="coolGray.200"
+      borderWidth="1"
+      _dark={{
+        borderColor: "coolGray.600",
+        backgroundColor: "gray.700",
+      }}
+      _web={{
+        shadow: 2,
+        borderWidth: 0,
+      }}
+      _light={{
+        backgroundColor: "gray.50",
+      }}
+      width="36px"
+      height="36px"
+    >
+      <HStack flex={1} justifyContent="center" alignItems="center">
+        {colorMode === "light" ? (
+          <Icon as={<Feather name="sun" size={20} color="black" />} size="md" />
+        ) : (
+          <Icon
+            as={
+              <MaterialCommunityIcons
+                name="weather-night"
+                size={20}
+                color="black"
+              />
+            }
+            size="md"
+          />
+        )}
+      </HStack>
+    </Pressable>
   );
 }
 
